b822edba3abc5f42cb898e9e0bf82d35b4cdcffd
tlstjrgml <zaqbnh741@gmail.com>
2026-06-29 11:21:44 +0900

    feat:내 신고내역

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -39,7 +39,11 @@ function Navbar({ isLoggedIn = false, nickname = '' , isAdmin = false}) {
               {isAdmin && (
                 <button className={`${styles.btn} ${styles.btnOutline}`} onClick={()=>{navigate('/admin')}}>관리자 페이지</button>
               )}
-
+              <button className={`${styles.btn} ${styles.btnOutline}`} onClick={()=>navigate('/mypage/reports')}><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
+                <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"/>
+                <path d="M13.73 21a2 2 0 0 1-3.46 0"/>
+              </svg>
+              </button>
               <button className={`${styles.btn} ${styles.btnOutline}`} onClick={()=>{navigate('/MyPage')}}>마이페이지</button>
               <button className={`${styles.btn} ${styles.btnGhost}`} onClick={()=>{
                 localStorage.removeItem('token');
